9769c0a781344c23029aeba47482fc18f10a87e9
Karthik06-ui <karthisundarakumar@gmail.com>
2026-02-28 10:19:07 +0530

    Update model loading to use dynamic file paths
    Refactor model and vectorizer loading to use dynamic paths.

--- ml/predict_logic.py
+++ ml/predict_logic.py
@@ -1,7 +1,13 @@
 import joblib
+import os
 
-model = joblib.load("models/fake_news_model.pkl")
-vectorizer = joblib.load("models/tfidf.pkl")
+BASE_DIR = os.path.dirname(__file__)
+
+model_path = os.path.join(BASE_DIR, "models", "fake_news_model.pkl")
+tfidf_path = os.path.join(BASE_DIR, "models", "tfidf.pkl")
+
+model = joblib.load(model_path)
+tfidf = joblib.load(tfidf_path)
 
 def predict_news(text):
     X = vectorizer.transform([text])
